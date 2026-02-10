60253f1c585bd7e689a9fd99c46172b7eadd0d07
Pablo Alvarez Barciela <pablalva@c1r10s6.42madrid.com>
2026-02-10 21:20:32 +0100

    solo toca pablo

---
+++ Parseo_(solo_toca_Pablo)/file_block.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file_block.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablalva <pablalva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/10 20:11:22 by pablalva          #+#    #+#             */
+/*   Updated: 2026/02/10 20:12:38 by pablalva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILE_BLOCK_HPP
+# define FILE_BLOCK_HPP
+# include <iostream>
+
+class file_block
+{
+private:
+	/* data */
+public:
+	file_block(/* args */);
+	~file_block();
+};
+
+file_block::file_block(/* args */)
+{
+}
+
+file_block::~file_block()
+{
+}
+
+
+#endif
