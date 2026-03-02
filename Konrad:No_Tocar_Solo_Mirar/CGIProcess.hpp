8ce4d07e2245041ed00fe21797c7922962a98d3b
Konrad Sudyn <ksudyn@c2r10s3.42madrid.com>
2026-03-02 21:29:45 +0100

    Inicio de CGIProces, a falta de verificar si fucniona lo hecho y empezar la ejecucion

--- Konrad:No_Tocar_Solo_Mirar/CGIProcess.hpp
+++ Konrad:No_Tocar_Solo_Mirar/CGIProcess.hpp
@@ -1,26 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   CGIProces.hpp                                      :+:      :+:    :+:   */
+/*   CGIProcess.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/18 16:55:57 by ksudyn            #+#    #+#             */
-/*   Updated: 2026/02/19 12:24:30 by ksudyn           ###   ########.fr       */
+/*   Created: 2026/03/02 21:20:08 by ksudyn            #+#    #+#             */
+/*   Updated: 2026/03/02 21:20:27 by ksudyn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "unistd.h"
+#include "RequestParser.hpp"
+#include "Block.hpp"
 
-class CGIProces
+class CGIProcess
 {
     private:
         int _inputPipe[2];
         int _uotputPippe[2];
         pid_t _pid;
+
+        std::string _cgiExtension;
+        std::string _cgiPass;
+        std::string _scriptPath;
+
+        void extractCGIConfig(const Block& location);
+        std::string extractExtension(const std::string& path);
     public:
+        bool isCGI(const Request& request, const Block& location);
         pid_t getPid() const;
 };
 
