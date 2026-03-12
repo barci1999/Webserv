707b833fc37ec51c96a592f18cbaf68fc47041b3
Pablo Alvarez Barciela <pablalva@c2r11s4.42madrid.com>
2026-03-12 15:24:19 +0100

    iniciao response

---
+++ Konrad:No_Tocar_Solo_Mirar/Response.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Response.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablalva <pablalva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 11:08:08 by pablalva          #+#    #+#             */
+/*   Updated: 2026/03/12 11:49:07 by pablalva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"Response.hpp"
+
+Response::Response(): _headers()
+{
+	this->_statusCode = 200;
+	this->_reasonPhrase = "OK";
+	this->_body = "";
+	this->_version="HTTP/1.1";
+}
+Response::Response(std::string version,unsigned int statucode,std::string reasonphrase,std::map<std::string,std::string> headers,std::string body)
+{
+	this->_version = version;
+	this->_statusCode = statucode;
+	this->_reasonPhrase = reasonphrase;
+	this->_headers = headers;
+	this->_body = body;
+}
+Response::Response(const Response& other)
+{
+	this->_version = other.get_version();
+	this->_statusCode = other.get_statusCode();
+	this->_reasonPhrase = other.get_reasonPhrase();
+	this->_headers = other.get_headers();
+	this->_body = other.get_body();
+}
+Response& Response::operator=(const Response& other)
+{
+	if(this == &other)
+		return *this;
+	this->_version = other.get_version();
+	this->_statusCode = other.get_statusCode();
+	this->_reasonPhrase = other.get_reasonPhrase();
+	this->_headers = other.get_headers();
+	this->_body = other.get_body();
+	return *this;
+}
+
---
+++ Konrad:No_Tocar_Solo_Mirar/Response.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Response.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pablalva <pablalva@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 17:51:11 by pablalva          #+#    #+#             */
+/*   Updated: 2026/03/12 11:46:08 by pablalva         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+#include<map>
+class Response
+{
+private:
+	std::string _version;
+	unsigned int _statusCode;
+	std::string _reasonPhrase;
+	std::map<std::string,std::string> _headers;
+	std::string _body;
+	
+public:
+	Response();
+	Response(const Response& other);
+	Response(std::string,unsigned int,std::string,std::map<std::string,std::string>,std::string);
+	Response &operator=(const Response& other);
+	~Response();
+
+	const std::string get_version() const {	return this->_version ;}
+	unsigned int get_statusCode() const { return this->_statusCode ;}
+	const std::string get_reasonPhrase() const {return this->_reasonPhrase;}
+	const std::map<std::string,std::string> get_headers() const {return this->_headers;}
+	const std::string get_body() const {return this->_body;}
+
+	void set_version(std::string version) {this->_version = version;}
+	void set_statuscode(unsigned int statuscode) {this->_statusCode = statuscode;}
+	void set_reasonphrase(std::string reasonphrase) {this->_reasonPhrase = reasonphrase;}
+	void set_Headers(std::map<std::string,std::string> headers) {this->_headers = headers;}
+	void addback_headers(std::string iterator,std::string value) {this->_headers[iterator] = value;}
+	void set_body(std::string body) {this->_body = body;}
+};
+
