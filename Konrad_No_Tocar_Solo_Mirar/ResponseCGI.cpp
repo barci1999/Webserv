8a8345b7612f0476864335ce6f85f320c04cf670
Konrad Sudyn <ksudyn@c3r8s5.42madrid.com>
2026-04-16 18:38:42 +0200

    cambios en setupChildProcess para que la ruta sea absoluta con un / al princiio, por que Pablo en su parseo lo quita y a él le funciona asi y a mi no, ordenada y comentada esa funcion. Tambien creado Response.cpp con la funcion de ejecucion de CGI dentro y la funcion handleRequest que no ira ahí al final y el main_cgi_test.cpp simplemente tuvo cambios por que al principio la funcion de ejecucuion de CGI estaba ahí, tambien tiene cosas, peros son de testeo, no para el proyecto final

---
+++ Konrad_No_Tocar_Solo_Mirar/ResponseCGI.cpp
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ResponseCGI.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 15:21:24 by ksudyn            #+#    #+#             */
+/*   Updated: 2026/04/15 18:38:17 by ksudyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "ResponseCGI.hpp"
+#include <map>
+#include <iostream>
+#include <unistd.h>
+#include <poll.h>
+
+std::vector<Response> CGI_Response(std::vector<Request>& requests, server& srv)
+{
+    std::vector<Response> responses;
+
+    std::vector<pollfd> pollfds;
+    std::map<int, CGIProcess*> cgi_map;
+
+    // 🔹 1. LANZAR TODOS LOS CGI
+    for (size_t i = 0; i < requests.size(); i++)
+    {
+        CGIProcess* cgi = new CGIProcess();
+
+        if (!cgi->isCGI(requests[i], srv))
+        {
+            delete cgi;
+            continue;
+        }
+
+        cgi->execute(requests[i], srv);
+
+        int fd = cgi->getFD();
+
+        struct pollfd p;
+        p.fd = fd;
+        p.events = POLLIN | POLLHUP;
+        p.revents = 0;
+
+        pollfds.push_back(p);
+        cgi_map[fd] = cgi;
+    }
+
+    // 🔥 2. LOOP ÚNICO (LA CLAVE)
+    while (!cgi_map.empty())
+    {
+        int ret = poll(&pollfds[0], pollfds.size(), -1);
+        if (ret < 0)
+        {
+            perror("poll");
+            break;
+        }
+
+        for (size_t i = 0; i < pollfds.size(); i++)
+        {
+            int fd = pollfds[i].fd;
+
+            // 🔴 TERMINADO
+            if (pollfds[i].revents & (POLLHUP | POLLERR))
+            {
+                CGIProcess* cgi = cgi_map[fd];
+
+                cgi->readFromPipe(); // leer lo último
+
+                Response res = cgi->buildResponse();
+                responses.push_back(res);
+
+                close(fd);
+                delete cgi;
+
+                cgi_map.erase(fd);
+                pollfds.erase(pollfds.begin() + i);
+                i--;
+                continue;
+            }
+
+            // 🟢 HAY DATOS
+            if (pollfds[i].revents & POLLIN)
+            {
+                CGIProcess* cgi = cgi_map[fd];
+                cgi->readFromPipe();
+            }
+        }
+    }
+
+    return responses;
+}
+
+
+
+//FUNCION QUE NO VA AQUI.
+//AQUI VERIFICO SI ES CGI Y SI LO ES, EJECUTO LO DEL CGI CON POLL Y SI NO RETORNO EL Response( req, srv);
+Response handleRequest(Request& req, server& srv)
+{
+    CGIProcess tmp;
+
+    // 🔴 1. ¿ES CGI?
+    if (tmp.isCGI(req, srv))
+    {
+        std::vector<Request> reqs;
+        reqs.push_back(req);
+
+        std::vector<Response> resps = CGI_Response(reqs, srv);
+
+        if (!resps.empty())
+            return resps[0];
+
+        // error CGI
+        Response error;
+        error.set_statuscode(500);
+        error.set_reasonphrase("Internal Server Error");
+        error.set_body("CGI failed");
+        return error;
+    }
+
+    // 🔵 2. NO CGI → usar el de tu compañero
+    return Response(req, srv);
+}
---
+++ Konrad_No_Tocar_Solo_Mirar/ResponseCGI.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ResponseCGI.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudyn <ksudyn@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/15 15:02:17 by ksudyn            #+#    #+#             */
+/*   Updated: 2026/04/15 18:22:54 by ksudyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "CGIProcess.hpp"
+#include "Request.hpp"
+#include "Response.hpp"
+#include "../Parseo_solo_toca_Pablo/server.hpp"
+
+#include <vector>
+
+//QUE TIENE QUE VER LA SQUERY CON EL CGI BUSCAR, POR QUE PABLO NO LAS USA
+
+std::vector<Response> CGI_Response(std::vector<Request>& requests, server& srv);
