9eed4d41622f177cd569675479eadb70883a22bb
Konrad Sudyn <ksudyn@c3r8s3.42madrid.com>
2026-04-23 21:36:35 +0200

    Nuevos .py. Me esta gustando, tengo ideas

--- Parseo_solo_toca_Pablo/www/test/cgi-bin/slow.py
+++ Parseo_solo_toca_Pablo/www/test/cgi-bin/slow.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python3
 import time
 import sys
+import os
 
 body = sys.stdin.read()
-
-print("Content-Type: text/plain")
-print()
-
-print("Empiezo...")
-time.sleep(5)
-
-print("Termino")
-print(body)
 
 print("Content-Type: text/html")
 print()
 
-print("<html><body>")
-print("<h1>🦜 Parrot CGI</h1>")
+print("<html><body style='font-family: Arial; text-align:center;'>")
 
-print("<h2>Method:</h2>")
-print("<p>%s</p>" % os.environ.get("REQUEST_METHOD", ""))
+print("<h1>🦜 PARROT CGI ACTIVADO 🦜</h1>")
 
-print("<h2>Query:</h2>")
-print("<p>%s</p>" % os.environ.get("QUERY_STRING", ""))
+print("<h2>⏳ Cargando sabiduría divina...</h2>")
+print()
+time.sleep(3)
 
-print("<h2>Body:</h2>")
-body = sys.stdin.read()
-print("<pre>%s</pre>" % body)
+print("<h2>💃 Cuando tu CGI fufona:</h2>")
+
+# GIF estilo Homer bailando (puedes cambiar la URL)
+
+print("<img src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTJvYzEwYXgyM21id3IyanIzbXM5YjZ6MHkyYzY3aXdvemo5ODVjdiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2nNI9yM6bxPxBhSCcA/giphy.gif' width='730'>")
 
 print("</body></html>")
--- Parseo_solo_toca_Pablo/www/test/cgi-bin/test.py
+++ Parseo_solo_toca_Pablo/www/test/cgi-bin/test.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
-
 import time
 import sys
 
 body = sys.stdin.read()
 
-# Primera cabecera: Status
 print("Status: 200 OK")
-print("Content-Type: text/plain")
+print("Content-Type: text/html")
 print()
-time.sleep(5)
-print("CGI funcionando!")
-print("Body recibido:")
-print(body)
-print("hola me llamo ramon y soy super listo")
+time.sleep(2)
+
+print("<h2>⌛ Procesando muy muy fuerte...<h2>")
+time.sleep(2)
+
+print("💡 Ahora sí. Ya lo tengo!")
+print()
+time.sleep(1)
+
+print("<h1>Como fan de Pokemon, te lo dedico Pablo<h1>")
+print("<img src='https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExeG1zbTdhdnEzbG44aDZhNzQ3cjhsczliOWxzNmhia2E3eTNpdXE2OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/frPnnZv9NJYfeldxr2/giphy.gif' width='730'>")
